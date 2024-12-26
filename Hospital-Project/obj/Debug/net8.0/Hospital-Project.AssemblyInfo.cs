a35cf083608fa94c9feb9f06694f8739276256de
horspaz064 <hormc1234@gmial.com>
2024-12-26 10:00:40 +0700

    formhor

--- Hospital-Project/obj/Debug/net8.0/Hospital-Project.AssemblyInfo.cs
+++ Hospital-Project/obj/Debug/net8.0/Hospital-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c3c3387e843d8a6d06db95105ade6b2692875b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
