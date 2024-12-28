a831ea9293e44122a0cc1ebac99c8b125ff015e0
vandy <vandy5144@gmail.com>
2024-12-28 10:24:12 +0700

    vandy

--- Hospital-Project/obj/Debug/net8.0/Hospital-Project.AssemblyInfo.cs
+++ Hospital-Project/obj/Debug/net8.0/Hospital-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad6466899dfdabdfceffefd80c8435b529953ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23585f0ae4d59f6ebd9b343a58086a135059b5a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
