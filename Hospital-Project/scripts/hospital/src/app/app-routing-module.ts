ddb6746f5d6810b5999f01072719de941f01b28e
horspaz064 <hormc1234@gmial.com>
2024-12-30 11:18:49 +0700

    commit

--- Hospital-Project/scripts/hospital/src/app/app-routing-module.ts
+++ Hospital-Project/scripts/hospital/src/app/app-routing-module.ts
@@ -8,9 +8,9 @@ import { AppComponent } from './app.component';
 const routes: Routes = [
     {
         path: '',
-        component: AppComponent,
-
-    }
+        component: AppComponent
+    },
+  
 ];
 
 @NgModule({
